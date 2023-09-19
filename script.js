09511b8c00f2f08dcfc1fc03f3f125c4f983fdf3
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-09-19 19:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
-
+let list = document.getElementById("infi-list");
+let count =11;
+list.addEventListener("scroll",(event)=>{
+	let currentScrollTop = list.scrollTop;
+	let scrollableHeight = list.scrollHeight - list.clientHeight;
+	
+	if(currentScrollTop===scrollableHeight){
+		let item1 = document.createElement("li");
+        let item2 = document.createElement("li");
+        item1.innerHTML = `Item ${count++}`;
+        item2.innerHTML = `Item ${count++}`;
+        list.append(item1, item2);
+	}
+	
+})
